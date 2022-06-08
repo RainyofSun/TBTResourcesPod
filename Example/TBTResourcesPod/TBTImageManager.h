d0986c50f194b00ade5104216caedd87cfee0c89
RainyofSun <159322316lr@#>
2022-06-08 15:00:39 +0800

    添加测试代码

---
+++ Example/TBTResourcesPod/TBTImageManager.h
@@ -0,0 +1,19 @@
+//
+//  TBTImageManager.h
+//  TBTResourcesPod_Example
+//
+//  Created by 刘冉 on 2022/6/8.
+//  Copyright © 2022 RainyofSun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TBTImageManager : NSObject
+
+- (void)getImg;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/TBTResourcesPod/TBTImageManager.m
@@ -0,0 +1,17 @@
+//
+//  TBTImageManager.m
+//  TBTResourcesPod_Example
+//
+//  Created by 刘冉 on 2022/6/8.
+//  Copyright © 2022 RainyofSun. All rights reserved.
+//
+
+#import "TBTImageManager.h"
+
+@implementation TBTImageManager
+
+- (void)getImg {
+    NSLog(@"获取图片资源");
+}
+
+@end
